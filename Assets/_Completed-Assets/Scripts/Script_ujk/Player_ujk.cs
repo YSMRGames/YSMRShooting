aa538dc834071c0bc008ccbf1878eb8333165980
Ujksan <ujkit114514810@gmail.com>
2018-02-25 01:03:32 +0900

    アニメーションの追加もれ、自機の移動処理の追加

---
+++ Assets/_Completed-Assets/Scripts/Script_ujk/Player_ujk.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_ujk : MonoBehaviour
+{
+    // 移動スピード
+    public float Speed = 5f;
+	
+	void Start ()
+    {
+
+	}
+	
+	
+	void Update ()
+    {
+        MovePlayer();
+	}
+
+    void MovePlayer()
+    {
+        // 右・左
+        float x = Input.GetAxisRaw("Horizontal");
+
+        // 上・下
+        float y = Input.GetAxisRaw("Vertical");
+
+        // 移動する向きを求める
+        Vector2 direction = new Vector2(x, y).normalized;
+
+        // 移動する向きとスピードを代入する
+        GetComponent<Rigidbody2D>().velocity = direction * Speed;
+
+    }
+}
